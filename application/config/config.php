2e351dba56a04cd14199043f7bbc6e8f47a42322
Gian Akbar <akbarigi20@gmail.com>
2023-10-03 12:26:15 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://[::1]:8888/sendok-garpu/';
+$config['base_url'] = 'http://[::1]:8888/wings/';
 
 /*
 |--------------------------------------------------------------------------
